4dc08caa02af44f7b2c89e1a91cc99466d91add1
Kaito-Dogi <kendobu0405@gmail.com>
2021-06-01 23:04:22 +0900

    feat: APIからデータを取得するInterfaseを実装しjsonデータを格納するためのデータクラスを作成した #15

---
+++ app/src/main/java/app/doggy/taskyumemi/RepositoryService.kt
@@ -0,0 +1,10 @@
+package app.doggy.taskyumemi
+
+import retrofit2.http.GET
+
+interface RepositoryService {
+
+    //ContributorのRepositoryを取得する
+    @GET("users/{login}/repos")
+    suspend fun getRepositories(): List<Repository>
+}
